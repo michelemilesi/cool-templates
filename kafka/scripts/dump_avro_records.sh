c0e0bd8fb5d89aa6006d0cc4fa01694e41c2a269
Michele Milesi <michele.milesi@icteam.it>
2023-12-13 09:56:05 +0100

    Update dump_avro_records.sh

--- kafka/scripts/dump_avro_records.sh
+++ kafka/scripts/dump_avro_records.sh
@@ -4,10 +4,13 @@ CONFIG_FILE=<config file>
 JAAS_FILE=<jaas file>
 SR_URL=<SCHEMA_REGISTRY_URL>
 
+topic=$1
+partition=$2
+
 export SCHEMA_REGISTRY_OPTS="-Djava.security.auth.login.config=${JAAS_FILE}"
 
 kafka-avro-console-consumer  --bootstrap-server=${BOOTSTRAP_SERVER} --consumer.config=$HOME/.kafka/admin.properties \ 
-  --topic $1 --partition $2 --property print.timestamp=true --property print.key=true --property print.offset=true \
+  --topic $topic --partition $partition --property print.timestamp=true --property print.key=true --property print.offset=true \
   --property print.partition=true --property print.headers=true --property print.value=false \
   --property  key.separator="!-----!" --property line.separator="!+++++!" --property headers.separator="!=====!" \
   --property key.converter=io.confluent.connect.avro.AvroConverter \
