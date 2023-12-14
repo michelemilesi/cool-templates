5a84bc925fc363dc5b1688b5ddf7507b9f1308f4
Michele Milesi <michele.milesi@icteam.it>
2023-12-14 17:53:47 +0000

    Readme

--- kafka/avro-console-client/avro-consumer.sh
+++ kafka/avro-console-client/avro-consumer.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 topic=$1
 partition=$2
+group=$3
 
 JAAS_FILE=<jaas file>
 CONFIG_FILE=config.properties
@@ -9,5 +10,5 @@ export KAFKA_OPTS="-Djava.security.auth.login.config=${JAAS_FILE}"
 export SCHEMA_REGISTRY_OPTS="-Djava.security.auth.login.config=${JAAS_FILE}"
 
 kafka-avro-console-consumer  --bootstrap-server=${BOOTSTRAP_SERVER} --consumer.config=${CONFIG_FILE} \ 
-  --topic $topic --partition $partition \
+  --topic $topic --partition $partition --group $group \
   --offset earliest --isolation-level read_committed | awk -v RS='!' 'print $0'
